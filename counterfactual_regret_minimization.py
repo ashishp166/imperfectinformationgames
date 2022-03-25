e496e3c444455c11b765f6762a2fee299a2635f4
Ashish Pandian <ashishpandian@berkeley.edu>
2022-03-25 15:23:32 -0700

    coin_toss

--- counterfactual_regret_minimization.py
+++ counterfactual_regret_minimization.py
@@ -1,6 +1,6 @@
 from Node import Node
 
-def cfr(nodes, game, gameState, p0, p1):
+def counterfactual_regret_minimization(nodes, game, gameState, p0, p1):
     #Player determination
     player = game.getCurrentPlayer(gameState)
 
@@ -10,16 +10,16 @@ def cfr(nodes, game, gameState, p0, p1):
     if game.isTerminal(gameState):
         opponent = 1 - player
         return game.getOutcome(gameState, player, opponent)
-    
+
 
     infoSet = game.getInfoSet(gameState)
     node = None
     if not infoSet in nodes:
-        noe = game.createNode(gameState)
+        node = game.createNode(gameState)
         nodes[infoSet] = node
     else:
         node = nodes[infoSet]
-    
+
 
     realizationWeight = [p0, p1][player]
     strategy = node.getStrategy(realizationWeight)
@@ -27,13 +27,13 @@ def cfr(nodes, game, gameState, p0, p1):
     nodeUtil = 0
     for a in range(node.getActionCount()):
         newState = gameState.nextHistory(a)
-        util[a] = -1.0 * cfr(ndoes, game, newState, [p0 * strategy[a], p0][player], [p1, p1 * strategy[a]][player])
+        util[a] = -1.0 * cfr(nodes, game, newState, [p0 * strategy[a], p0][player], [p1, p1 * strategy[a]][player])
         nodeUtil += strategy[a] * util[a]
-    
+
 
     for a in range(node.getActionCount()):
         regret = util[a] - nodeUtil
-        node.regret[a] += [p1, p0][player] * regret
-    
+        node.regretSum[a] += [p1, p0][player] * regret
+
 
     return nodeUtil
